a5002f34c123fe6719a5dc21c584c32b4947edb0
kenny <97374837+kankenny@users.noreply.github.com>
2024-08-30 00:27:16 +0000

    feat: upload model and loading of it

--- tests/test_endpt.py
+++ tests/test_endpt.py
@@ -4,3 +4,10 @@ def placeholder_value():
 
 def test_endpoint():
     assert placeholder_value
+
+
+def test_model_existence():
+    import os
+
+    file_path = os.path.join(os.getcwd(), 'ecg_model.pkl')
+    assert os.path.isfile(file_path), f"File not found: {file_path}"
