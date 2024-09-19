ad60df3a73eccf21ad6078b3834e4f25bf5d1601
kenny <97374837+kankenny@users.noreply.github.com>
2024-09-19 23:35:08 +0000

    chore: modularize util file

--- endpoint.py
+++ endpoint.py
@@ -2,7 +2,7 @@ import joblib
 import pandas as pd
 import sys
 from fastapi import FastAPI, HTTPException
-from util import ECGData, rename_columns
+from util.util import ECGData, rename_columns
 
 
 mode = sys.argv[1] if len(sys.argv) > 1 else 'dev'
