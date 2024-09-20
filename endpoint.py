7ba848c5d817b79c6f1027cb4163e62a2c9b3c00
kenny <97374837+kankenny@users.noreply.github.com>
2024-09-20 17:19:09 +0000

    feat: implemenet dockerfiles for dev and prod envs

--- endpoint.py
+++ endpoint.py
@@ -1,11 +1,16 @@
 import joblib
 import pandas as pd
 import sys
+
 from fastapi import FastAPI, HTTPException
+from model.transformers import * 
 from util.util import ECGData, rename_columns
 
 
-mode = sys.argv[1] if len(sys.argv) > 1 else 'dev'
+mode = sys.argv[1] if len(sys.argv) > 1 else "dev"
+
+if mode not in ["dev", "prod"]:
+    raise EnvironmentError(f"Invalid mode: {mode}. Expected 'dev' or 'prod'.")
 
 app = FastAPI()
 
